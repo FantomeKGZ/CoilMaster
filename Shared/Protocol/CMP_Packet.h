403e3a5b18a63f580569d948715255d8796e551d
FantomeKGZ <deadgrog@gmail.com>
2026-08-03 22:36:00 +0600

    refactor(protocol): rebuild packet data model

--- Shared/Protocol/CMP_Packet.h
+++ Shared/Protocol/CMP_Packet.h
@@ -1,16 +1,3 @@
-/*
-==========================================================
-CoilMaster OS
-CMP (CoilMaster Protocol)
-
-File      : CMP_Packet.h
-Module    : Shared/Protocol
-
-Description:
-Packet Definition
-==========================================================
-*/
-
 #ifndef CMP_PACKET_H
 #define CMP_PACKET_H
 
@@ -19,36 +6,14 @@ Packet Definition
 #include "CMP_Defines.h"
 #include "CMP_Header.h"
 
-#pragma pack(push,1)
-
-struct CMP_Packet
+namespace CMP
 {
-    //------------------------------------------------------
-    // Header
-    //------------------------------------------------------
-
-    CMP_Header header;
-
-    //------------------------------------------------------
-    // Payload
-    //------------------------------------------------------
-
-    uint8_t payload[CMP_MAX_PAYLOAD_SIZE];
-
-    //------------------------------------------------------
-    // CRC16
-    //------------------------------------------------------
-
+struct Packet
+{
+    Header header;
+    uint8_t payload[MaxPayloadSize];
     uint16_t crc;
 };
+}
 
-#pragma pack(pop)
-
-static_assert(
-    sizeof(CMP_Packet) ==
-    sizeof(CMP_Header)
-    + CMP_MAX_PAYLOAD_SIZE
-    + sizeof(uint16_t),
-    "CMP_Packet size mismatch");
-
-#endif
+#endif // CMP_PACKET_H

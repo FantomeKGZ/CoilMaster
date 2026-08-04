22fcc4127abcf64d7dadfd1b2cc8d33e4dfbf53c
FantomeKGZ <deadgrog@gmail.com>
2026-08-04 14:08:05 +0600

    Add Arduino Uno hardware pin configuration

---
+++ Arduino/Config/CM_Pins.h
@@ -0,0 +1,31 @@
+#ifndef CM_PINS_H
+#define CM_PINS_H
+
+#include <Arduino.h>
+
+namespace CM
+{
+namespace Pins
+{
+constexpr uint8_t KeypadRow0 = 2U;
+constexpr uint8_t KeypadRow1 = 3U;
+constexpr uint8_t KeypadRow2 = 4U;
+constexpr uint8_t KeypadRow3 = 5U;
+constexpr uint8_t KeypadCol0 = 6U;
+constexpr uint8_t KeypadCol1 = 7U;
+constexpr uint8_t KeypadCol2 = 8U;
+constexpr uint8_t KeypadCol3 = 9U;
+
+constexpr uint8_t StartButton = 10U;
+constexpr uint8_t Buzzer = 11U;
+constexpr uint8_t Ssr = 12U;
+
+constexpr uint8_t Hall = A0;
+constexpr uint8_t EspTx = A1;
+constexpr uint8_t EspRx = A2;
+
+constexpr uint8_t LcdI2cAddress = 0x27U;
+} // namespace Pins
+} // namespace CM
+
+#endif // CM_PINS_H

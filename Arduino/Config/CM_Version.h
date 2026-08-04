38b6537d36b9b14d3944fde3e87a755d694f3143
FantomeKGZ <deadgrog@gmail.com>
2026-08-04 13:18:44 +0600

    feat: add firmware version metadata

---
+++ Arduino/Config/CM_Version.h
@@ -0,0 +1,10 @@
+#ifndef CM_VERSION_H
+#define CM_VERSION_H
+
+#define CM_VERSION_MAJOR 0
+#define CM_VERSION_MINOR 2
+#define CM_VERSION_PATCH 0
+#define CM_VERSION_LABEL "arduino-core-dev"
+#define CM_VERSION_STRING "0.2.0-arduino-core-dev"
+
+#endif // CM_VERSION_H

e24395fe0b1aa5015c883b5be54d1a4bca83974b
FantomeKGZ <deadgrog@gmail.com>
2026-08-05 21:46:41 +0600

    feat(costing): declare repair costing API

---
+++ firmware/esp32/src/CM_RepairCostingWeb.h
@@ -0,0 +1,28 @@
+#ifndef CM_REPAIR_COSTING_WEB_H
+#define CM_REPAIR_COSTING_WEB_H
+
+#include <Arduino.h>
+#include <WebServer.h>
+#include "CM_RepairCosting.h"
+
+namespace CM
+{
+class RepairCostingWeb
+{
+public:
+    RepairCostingWeb(WebServer& server, RepairCosting& costing);
+    void begin();
+
+private:
+    void handleGet();
+    void handleSavePricing();
+    static bool parseUnsigned(WebServer& server,const char* name,uint32_t minValue,uint32_t maxValue,uint32_t& value);
+    static bool parseUnsigned64(WebServer& server,const char* name,uint64_t& value);
+    static void appendUInt64(String& target,uint64_t value);
+
+    WebServer& m_server;
+    RepairCosting& m_costing;
+};
+}
+
+#endif

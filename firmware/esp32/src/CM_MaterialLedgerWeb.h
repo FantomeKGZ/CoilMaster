a61de982830ed21fb1908d54332728862808348c
FantomeKGZ <deadgrog@gmail.com>
2026-08-05 21:38:04 +0600

    feat(materials): declare materials ledger API

---
+++ firmware/esp32/src/CM_MaterialLedgerWeb.h
@@ -0,0 +1,33 @@
+#ifndef CM_MATERIAL_LEDGER_WEB_H
+#define CM_MATERIAL_LEDGER_WEB_H
+
+#include <Arduino.h>
+#include <WebServer.h>
+
+#include "CM_MaterialLedger.h"
+
+namespace CM
+{
+class MaterialLedgerWeb
+{
+public:
+    MaterialLedgerWeb(WebServer& server, MaterialLedger& ledger);
+    void begin();
+
+private:
+    void handleList();
+    void handleCreate();
+    void handleUsage();
+    static bool parseUnsigned(WebServer& server,
+                              const char* name,
+                              uint32_t minimum,
+                              uint32_t maximum,
+                              uint32_t& value);
+    static bool parseUnit(const String& source, MaterialUnit& unit);
+
+    WebServer& m_server;
+    MaterialLedger& m_ledger;
+};
+}
+
+#endif // CM_MATERIAL_LEDGER_WEB_H

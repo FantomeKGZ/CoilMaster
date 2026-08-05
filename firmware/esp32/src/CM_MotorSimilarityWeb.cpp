d56a19b276337b415425f5a6ebcfe9cce71be4e8
FantomeKGZ <deadgrog@gmail.com>
2026-08-05 23:46:31 +0600

    feat(motors): expose similarity lookup API

---
+++ firmware/esp32/src/CM_MotorSimilarityWeb.cpp
@@ -0,0 +1,55 @@
+#include "CM_MotorSimilarityWeb.h"
+
+namespace CM
+{
+MotorSimilarityWeb::MotorSimilarityWeb(WebServer& server, RepairRegistry& registry)
+    : m_server(server), m_registry(registry) {}
+
+void MotorSimilarityWeb::begin()
+{
+    m_server.on("/api/motors/similar", HTTP_GET,
+                [this]() { handleLookup(); });
+}
+
+void MotorSimilarityWeb::handleLookup()
+{
+    if (!m_registry.ready())
+    {
+        m_server.send(503, "application/json; charset=utf-8",
+                      "{\"error\":\"repair_registry_unavailable\"}");
+        return;
+    }
+    if (!m_server.hasArg("coil_program") ||
+        m_server.arg("coil_program").length() == 0U)
+    {
+        m_server.send(400, "application/json; charset=utf-8",
+                      "{\"error\":\"coil_program_required\"}");
+        return;
+    }
+
+    NewMotor candidate;
+    candidate.name = m_server.arg("name");
+    candidate.model = m_server.arg("model");
+    candidate.manufacturer = m_server.arg("manufacturer");
+    candidate.coilProgram = m_server.arg("coil_program");
+
+    String response = F("{\"items\":[");
+    response.reserve(4096U);
+    uint16_t sameProgramCount = 0U;
+    uint16_t identityMatchCount = 0U;
+    if (!m_registry.appendSimilarMotorsJson(response, candidate,
+                                            sameProgramCount,
+                                            identityMatchCount))
+    {
+        m_server.send(500, "application/json; charset=utf-8",
+                      "{\"error\":\"similarity_lookup_failed\"}");
+        return;
+    }
+    response += F("],\"same_program_count\":");
+    response += sameProgramCount;
+    response += F(",\"identity_match_count\":");
+    response += identityMatchCount;
+    response += F(",\"creation_blocked\":false}");
+    m_server.send(200, "application/json; charset=utf-8", response);
+}
+}

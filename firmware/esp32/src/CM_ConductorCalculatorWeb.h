ee0324ba90aca47e16cd88b0fc65d0e2f1e0e94b
FantomeKGZ <deadgrog@gmail.com>
2026-08-05 21:11:09 +0600

    feat(calculator): expose persistent settings API

--- firmware/esp32/src/CM_ConductorCalculatorWeb.h
+++ firmware/esp32/src/CM_ConductorCalculatorWeb.h
@@ -3,7 +3,6 @@
 
 #include <Arduino.h>
 #include <WebServer.h>
-
 #include "CM_ConductorCalculator.h"
 #include "CM_WarehouseStore.h"
 
@@ -17,18 +16,14 @@ public:
 
 private:
     void handleCalculate();
-    static bool parseUnsignedArg(WebServer& server,
-                                 const char* name,
-                                 uint32_t minimum,
-                                 uint32_t maximum,
-                                 uint32_t& value);
-    static bool parseMaterial(const String& value, ConductorMaterial& material);
+    void handleGetSettings();
+    void handleSetSettings();
+    static bool parseUnsignedArg(WebServer& server,const char* name,uint32_t minimum,uint32_t maximum,uint32_t& value);
+    static bool parseMaterial(const String& value,ConductorMaterial& material);
     static const char* materialText(ConductorMaterial material);
     static const char* availabilityText(ConversionAvailability availability);
-
     WebServer& m_server;
     WarehouseStore& m_warehouse;
 };
 }
-
-#endif // CM_CONDUCTOR_CALCULATOR_WEB_H
+#endif

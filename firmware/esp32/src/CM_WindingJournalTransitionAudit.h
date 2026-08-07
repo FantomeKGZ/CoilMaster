adafc73064e85f9780cd6d088dbe9e53fd960a68
FantomeKGZ <deadgrog@gmail.com>
2026-08-07 23:31:28 +0600

    Add winding journal transition audit contract

---
+++ firmware/esp32/src/CM_WindingJournalTransitionAudit.h
@@ -0,0 +1,23 @@
+#ifndef CM_WINDING_JOURNAL_TRANSITION_AUDIT_H
+#define CM_WINDING_JOURNAL_TRANSITION_AUDIT_H
+
+#include <Arduino.h>
+#include <FS.h>
+
+namespace CM
+{
+enum class WindingJournalTransitionAuditResult : uint8_t
+{
+    Ok = 0,
+    StorageUnavailable = 1,
+    ReadFailed = 2
+};
+
+class WindingJournalTransitionAudit
+{
+public:
+    static WindingJournalTransitionAuditResult validate(fs::FS& storage);
+};
+}
+
+#endif // CM_WINDING_JOURNAL_TRANSITION_AUDIT_H

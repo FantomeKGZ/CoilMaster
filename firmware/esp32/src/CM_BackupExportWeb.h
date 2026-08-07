14de532bb66c7e5536f596703c18c769270418dc
FantomeKGZ <deadgrog@gmail.com>
2026-08-08 00:13:51 +0600

    Add read only backup export web contract

---
+++ firmware/esp32/src/CM_BackupExportWeb.h
@@ -0,0 +1,27 @@
+#ifndef CM_BACKUP_EXPORT_WEB_H
+#define CM_BACKUP_EXPORT_WEB_H
+
+#include <Arduino.h>
+#include <FS.h>
+#include <WebServer.h>
+
+namespace CM
+{
+class BackupExportWeb
+{
+public:
+    BackupExportWeb(WebServer& server, fs::FS& storage);
+
+    void begin();
+    bool ready() const;
+
+private:
+    void handleManifest();
+    void handleFile();
+
+    WebServer& m_server;
+    fs::FS& m_storage;
+};
+}
+
+#endif // CM_BACKUP_EXPORT_WEB_H

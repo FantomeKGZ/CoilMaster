cf7df132d190bd359a4f4b85b2553f6dcdba5dd4
FantomeKGZ <deadgrog@gmail.com>
2026-08-08 19:07:02 +0600

    Expose business data audit counts

--- firmware/esp32/src/CM_BackupBusinessDataIntegrityAudit.h
+++ firmware/esp32/src/CM_BackupBusinessDataIntegrityAudit.h
@@ -2,13 +2,24 @@
 #define CM_BACKUP_BUSINESS_DATA_INTEGRITY_AUDIT_H
 
 #include <FS.h>
+#include <stdint.h>
 
 namespace CM
 {
+struct BackupBusinessDataAuditMetrics
+{
+    uint32_t clientRecordCount = 0UL;
+    uint32_t motorRecordCount = 0UL;
+    uint32_t repairRecordCount = 0UL;
+    uint32_t repairStatusRecordCount = 0UL;
+    uint32_t pricingRecordCount = 0UL;
+};
+
 class BackupBusinessDataIntegrityAudit
 {
 public:
     static bool check(fs::FS& storage);
+    static bool check(fs::FS& storage, BackupBusinessDataAuditMetrics& metrics);
 };
 }
 

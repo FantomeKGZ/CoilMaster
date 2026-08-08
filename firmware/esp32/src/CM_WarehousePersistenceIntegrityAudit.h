fdb2428895004b7f248504bab8ef85334651535a
FantomeKGZ <deadgrog@gmail.com>
2026-08-08 19:16:49 +0600

    Expose warehouse persistence audit counts

--- firmware/esp32/src/CM_WarehousePersistenceIntegrityAudit.h
+++ firmware/esp32/src/CM_WarehousePersistenceIntegrityAudit.h
@@ -2,13 +2,21 @@
 #define CM_WAREHOUSE_PERSISTENCE_INTEGRITY_AUDIT_H
 
 #include <FS.h>
+#include <stdint.h>
 
 namespace CM
 {
+struct WarehousePersistenceAuditMetrics
+{
+    uint32_t spoolRecordCount = 0UL;
+    uint32_t priceRecordCount = 0UL;
+};
+
 class WarehousePersistenceIntegrityAudit
 {
 public:
     static bool check(fs::FS& storage);
+    static bool check(fs::FS& storage, WarehousePersistenceAuditMetrics& metrics);
 };
 }
 

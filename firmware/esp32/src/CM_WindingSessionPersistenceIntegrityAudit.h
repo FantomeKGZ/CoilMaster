9c33178d8b580460e1d34962322fe81b9771dccc
FantomeKGZ <deadgrog@gmail.com>
2026-08-08 19:06:01 +0600

    Expose winding session persistence counts

--- firmware/esp32/src/CM_WindingSessionPersistenceIntegrityAudit.h
+++ firmware/esp32/src/CM_WindingSessionPersistenceIntegrityAudit.h
@@ -2,13 +2,23 @@
 #define CM_WINDING_SESSION_PERSISTENCE_INTEGRITY_AUDIT_H
 
 #include <FS.h>
+#include <stdint.h>
 
 namespace CM
 {
+struct WindingSessionPersistenceAuditMetrics
+{
+    uint32_t snapshotFileCount = 0UL;
+    uint32_t stateFileCount = 0UL;
+    uint32_t spoolSelectionFileCount = 0UL;
+};
+
 class WindingSessionPersistenceIntegrityAudit
 {
 public:
     static bool check(fs::FS& storage);
+    static bool check(fs::FS& storage,
+                      WindingSessionPersistenceAuditMetrics& metrics);
 };
 }
 

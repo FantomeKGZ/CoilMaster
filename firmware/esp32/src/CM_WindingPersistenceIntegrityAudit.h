11191769c1a8bade30f4fa2aa88dda04e7702158
FantomeKGZ <deadgrog@gmail.com>
2026-08-08 08:45:38 +0600

    Add winding persistence integrity audit contract

---
+++ firmware/esp32/src/CM_WindingPersistenceIntegrityAudit.h
@@ -0,0 +1,15 @@
+#ifndef CM_WINDING_PERSISTENCE_INTEGRITY_AUDIT_H
+#define CM_WINDING_PERSISTENCE_INTEGRITY_AUDIT_H
+
+#include <FS.h>
+
+namespace CM
+{
+class WindingPersistenceIntegrityAudit
+{
+public:
+    static bool check(fs::FS& storage);
+};
+}
+
+#endif // CM_WINDING_PERSISTENCE_INTEGRITY_AUDIT_H

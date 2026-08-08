ac031d8cc14a74786e10c8adb782776b0d16e97f
FantomeKGZ <deadgrog@gmail.com>
2026-08-08 18:53:03 +0600

    Expose material persistence audit counts

--- firmware/esp32/src/CM_MaterialPersistenceIntegrityAudit.h
+++ firmware/esp32/src/CM_MaterialPersistenceIntegrityAudit.h
@@ -2,13 +2,22 @@
 #define CM_MATERIAL_PERSISTENCE_INTEGRITY_AUDIT_H
 
 #include <FS.h>
+#include <stdint.h>
 
 namespace CM
 {
+struct MaterialPersistenceAuditMetrics
+{
+    uint32_t materialRecordCount = 0UL;
+    uint32_t usageRecordCount = 0UL;
+    uint32_t adjustmentRecordCount = 0UL;
+};
+
 class MaterialPersistenceIntegrityAudit
 {
 public:
     static bool check(fs::FS& storage);
+    static bool check(fs::FS& storage, MaterialPersistenceAuditMetrics& metrics);
 };
 }
 

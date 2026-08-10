200138ad3dc205b07af3791d266e76b1d3008d32
FantomeKGZ <deadgrog@gmail.com>
2026-08-10 12:26:23 +0600

    Add autonomous winding archive web API interface

---
+++ firmware/esp32/src/CM_AutonomousWindingWeb.h
@@ -0,0 +1,32 @@
+#ifndef CM_AUTONOMOUS_WINDING_WEB_H
+#define CM_AUTONOMOUS_WINDING_WEB_H
+
+#include <Arduino.h>
+#include <WebServer.h>
+
+#include "CM_AutonomousWindingArchive.h"
+#include "CM_RepairRegistry.h"
+
+namespace CM
+{
+class AutonomousWindingWeb
+{
+public:
+    AutonomousWindingWeb(WebServer& server,
+                         AutonomousWindingArchive& archive,
+                         RepairRegistry& registry);
+
+    void begin();
+
+private:
+    void handleList();
+    void handleAssign();
+    static bool parseCanonicalUint32(const String& text, uint32_t& value);
+
+    WebServer& m_server;
+    AutonomousWindingArchive& m_archive;
+    RepairRegistry& m_registry;
+};
+}
+
+#endif

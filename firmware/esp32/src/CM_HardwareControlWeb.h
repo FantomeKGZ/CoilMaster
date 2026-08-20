607e89d3790381611c7b3584ae637a1f095fc3e0
FantomeKGZ <deadgrog@gmail.com>
2026-08-20 11:33:01 +0600

    feat: add ESP32 Hall hardware HTTP interface

---
+++ firmware/esp32/src/CM_HardwareControlWeb.h
@@ -0,0 +1,53 @@
+#ifndef CM_HARDWARE_CONTROL_WEB_H
+#define CM_HARDWARE_CONTROL_WEB_H
+
+#include <Arduino.h>
+#include <WebServer.h>
+
+#include "CM_UartEventReceiver.h"
+
+namespace CM
+{
+
+class HardwareControlWeb
+{
+public:
+    HardwareControlWeb(WebServer& server, UartEventReceiver& receiver);
+
+    void begin();
+    void update(uint32_t nowMs);
+
+private:
+    static constexpr uint32_t SettingsFreshMs = 15000UL;
+    static constexpr uint32_t TelemetryFreshMs = 1500UL;
+
+    void handleState();
+    void handleRefresh();
+    void handleSave();
+    void handleReset();
+    void handleTelemetryState();
+    void handleTelemetryStart();
+    void handleTelemetryStop();
+
+    bool queueAccepted(bool accepted, const char* errorName);
+    static bool parseUnsigned(const String& source,
+                              uint32_t maximum,
+                              uint32_t& value);
+    static const char* directionText(HallSignalDirectionRemote direction);
+    static const char* replyText(HardwareControlReplyResult result);
+    static bool fresh(uint32_t nowMs, uint32_t receivedAtMs, uint32_t limitMs);
+
+    WebServer& m_server;
+    UartEventReceiver& m_receiver;
+    HallSettingsState m_settings;
+    HallTelemetryState m_telemetry;
+    HardwareControlReply m_reply;
+    bool m_hasSettings;
+    bool m_hasTelemetry;
+    bool m_hasReply;
+    uint32_t m_nowMs;
+};
+
+} // namespace CM
+
+#endif // CM_HARDWARE_CONTROL_WEB_H

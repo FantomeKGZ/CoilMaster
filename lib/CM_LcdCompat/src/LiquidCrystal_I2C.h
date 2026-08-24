7479e69572893245181e087205f78cb17182f84a
FantomeKGZ <deadgrog@gmail.com>
2026-08-24 12:31:39 +0600

    build(arduino): add LCD compatibility shim

---
+++ lib/CM_LcdCompat/src/LiquidCrystal_I2C.h
@@ -0,0 +1,8 @@
+#ifndef CM_LCD_COMPAT_LIQUIDCRYSTAL_I2C_H
+#define CM_LCD_COMPAT_LIQUIDCRYSTAL_I2C_H
+
+#include "../../../Arduino/CM_Pcf8574Lcd.h"
+
+using LiquidCrystal_I2C = CM::Pcf8574Lcd;
+
+#endif

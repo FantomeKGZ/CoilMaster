beba352891c312fc5486e7e4729bc04016815bfb
FantomeKGZ <deadgrog@gmail.com>
2026-08-25 10:56:56 +0600

    perf(esp32): make winding persistence single pass

--- firmware/esp32/src/CM_WindingPersistenceIntegrityAudit.cpp
+++ firmware/esp32/src/CM_WindingPersistenceIntegrityAudit.cpp
@@ -1,5 +1,4 @@
 #include "CM_WindingPersistenceIntegrityAudit.h"
-#include "CM_WindingJournalQuery.h"
 #include "CM_WindingJournalTransitionAudit.h"
 
 namespace CM
@@ -14,21 +13,7 @@ bool WindingPersistenceIntegrityAudit::check(fs::FS& storage,
                                              uint32_t& recordCount)
 {
     recordCount = 0UL;
-    WindingJournalQuery query(storage);
-    uint32_t validatedRecordCount = 0UL;
-    if (!query.begin() ||
-        query.validateAll(validatedRecordCount) != WindingJournalQueryResult::Ok)
-    {
-        return false;
-    }
-
-    if (WindingJournalTransitionAudit::validate(storage) !=
-        WindingJournalTransitionAuditResult::Ok)
-    {
-        return false;
-    }
-
-    recordCount = validatedRecordCount;
-    return true;
+    return WindingJournalTransitionAudit::validate(storage, recordCount) ==
+           WindingJournalTransitionAuditResult::Ok;
 }
 }

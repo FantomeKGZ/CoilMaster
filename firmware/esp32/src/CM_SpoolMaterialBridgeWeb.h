b70bb146669e547071a6506745900f1685dcec72
FantomeKGZ <deadgrog@gmail.com>
2026-08-26 13:16:30 +0600

    feat(warehouse): add operator spool bridge web contract

---
+++ firmware/esp32/src/CM_SpoolMaterialBridgeWeb.h
@@ -0,0 +1,41 @@
+#ifndef CM_SPOOL_MATERIAL_BRIDGE_WEB_H
+#define CM_SPOOL_MATERIAL_BRIDGE_WEB_H
+
+#include <WebServer.h>
+
+#include "CM_MaterialLedger.h"
+#include "CM_SpoolMaterialBridgeStore.h"
+#include "CM_WarehouseStore.h"
+
+namespace CM
+{
+class SpoolMaterialBridgeWeb
+{
+public:
+    SpoolMaterialBridgeWeb(WebServer& server,
+                           WarehouseStore& warehouse,
+                           MaterialLedger& materials,
+                           SpoolMaterialBridgeStore& bridges)
+        : m_server(server), m_warehouse(warehouse), m_materials(materials),
+          m_bridges(bridges)
+    {
+    }
+
+    void begin();
+
+private:
+    void handleCreate();
+    static bool parseUnsigned(WebServer& server,
+                              const char* name,
+                              uint32_t minimum,
+                              uint32_t maximum,
+                              uint32_t& value);
+
+    WebServer& m_server;
+    WarehouseStore& m_warehouse;
+    MaterialLedger& m_materials;
+    SpoolMaterialBridgeStore& m_bridges;
+};
+}
+
+#endif // CM_SPOOL_MATERIAL_BRIDGE_WEB_H

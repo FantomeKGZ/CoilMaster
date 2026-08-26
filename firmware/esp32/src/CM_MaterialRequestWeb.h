c90ddffee29a5cf7bf0775b42b9c1bba0eb85f3d
FantomeKGZ <deadgrog@gmail.com>
2026-08-26 09:18:07 +0600

    feat(crm): add material request web API interface

---
+++ firmware/esp32/src/CM_MaterialRequestWeb.h
@@ -0,0 +1,53 @@
+#ifndef CM_MATERIAL_REQUEST_WEB_H
+#define CM_MATERIAL_REQUEST_WEB_H
+
+#include <Arduino.h>
+#include <WebServer.h>
+
+#include "CM_MaterialRequestMovementStore.h"
+#include "CM_MaterialRequestStatusStore.h"
+#include "CM_MaterialRequestStore.h"
+#include "CM_MaterialRequestWarehouseCoordinator.h"
+#include "CM_RepairRegistry.h"
+
+namespace CM
+{
+class MaterialRequestWeb
+{
+public:
+    MaterialRequestWeb(WebServer& server,
+                       RepairRegistry& repairs,
+                       MaterialRequestStore& requests,
+                       MaterialRequestMovementStore& movements,
+                       MaterialRequestStatusStore& statuses,
+                       MaterialRequestWarehouseCoordinator& warehouse);
+
+    void begin();
+
+private:
+    void handleCreate();
+    void handleGetById();
+    void handleListByRepair();
+    void handleMovements();
+    void handleStatus();
+    void handleTransition();
+    void handleWarehouseAction();
+
+    static bool parseUnsigned(WebServer& server,
+                              const char* name,
+                              uint32_t minimum,
+                              uint32_t maximum,
+                              uint32_t& value);
+    static bool validTimestamp(const String& value);
+    static void appendUint64(String& target, uint64_t value);
+
+    WebServer& m_server;
+    RepairRegistry& m_repairs;
+    MaterialRequestStore& m_requests;
+    MaterialRequestMovementStore& m_movements;
+    MaterialRequestStatusStore& m_statuses;
+    MaterialRequestWarehouseCoordinator& m_warehouse;
+};
+}
+
+#endif // CM_MATERIAL_REQUEST_WEB_H

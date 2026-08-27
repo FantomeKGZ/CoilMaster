a55530abafd3edda6c730aef7abb458b43d30141
FantomeKGZ <deadgrog@gmail.com>
2026-08-27 12:04:42 +0600

    perf(warehouse): share unsigned value parser

--- firmware/esp32/src/CM_WarehouseWeb.h
+++ firmware/esp32/src/CM_WarehouseWeb.h
@@ -45,6 +45,10 @@ private:
                                  uint32_t minimum,
                                  uint32_t maximum,
                                  uint32_t& value);
+    static bool parseUnsignedValue(const String& source,
+                                   uint32_t minimum,
+                                   uint32_t maximum,
+                                   uint32_t& value);
 
     WebServer& m_server;
     WarehouseStore& m_store;

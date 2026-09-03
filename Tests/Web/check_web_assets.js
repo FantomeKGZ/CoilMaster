c4f6ad4cbeea54ce688f8457837c9862185179ee
FantomeKGZ <deadgrog@gmail.com>
2026-09-03 11:29:05 +0600

    test(web): include repair material parity audit

--- Tests/Web/check_web_assets.js
+++ Tests/Web/check_web_assets.js
@@ -1,6 +1,7 @@
 require('./check_client_new_ui.js');
 require('./check_web_recovery_ftp_contracts.js');
 require('./check_remote_backup_ui_parity.js');
+require('./check_repair_material_card_ui.js');
 
 const fs = require('fs');
 const path = require('path');
